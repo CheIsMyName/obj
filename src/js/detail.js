f8f7f2f4ff2c5c361dbdd426f329ec91f9a10a59
CheIsMyName <985046147@qq.com>
2018-06-07 17:17:46 +0800

    '放大镜已完成'

---
+++ src/js/detail.js
@@ -0,0 +1,44 @@
+$(function(){
+			
+			$('#a2').click(function(){
+				$('#s-img2').css('display','block');
+				$('#b-img2').css('display','block');
+				$('#s-img1').css('display','none');
+				$('#b-img1').css('display','none');
+				$('#s-img3').css('display','none');
+				$('#b-img3').css('display','none');
+				$('#s-img4').css('display','none');
+				$('#b-img4').css('display','none')
+			})
+			$('#a1').click(function(){
+				$('#s-img1').css('display','block');
+				$('#b-img1').css('display','block');
+				$('#s-img2').css('display','none');
+				$('#b-img2').css('display','none');
+				$('#s-img3').css('display','none');
+				$('#b-img3').css('display','none');
+				$('#s-img4').css('display','none');
+				$('#b-img4').css('display','none');
+			})
+			$('#a3').click(function(){
+				$('#s-img3').css('display','block');
+				$('#b-img3').css('display','block');
+				$('#s-img2').css('display','none');
+				$('#b-img2').css('display','none');
+				$('#s-img1').css('display','none');
+				$('#b-img1').css('display','none');
+				$('#s-img4').css('display','none');
+				$('#b-img4').css('display','none');
+			})
+			$('#a4').click(function(){
+				$('#s-img4').css('display','block');
+				$('#b-img4').css('display','block');
+				$('#s-img2').css('display','none');
+				$('#b-img2').css('display','none')
+				$('#s-img1').css('display','none');
+				$('#b-img1').css('display','none')
+				$('#s-img3').css('display','none');
+				$('#b-img3').css('display','none')
+			})
+		})
+	
